0e1f8f18a9b7650d68d251ed7e540057ecdd15f5
Mahir Shahriyar Sezan <mahirsezan@gmail.com>
2019-09-10 13:48:22 +0600

    Added Siam

--- Siam On a Quest/Siam On a Quest/Siam On a Quest.c
+++ Siam On a Quest/Siam On a Quest/Siam On a Quest.c
@@ -1,15 +1,22 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #define F_CPU 1000000
-#define INF 1e9
 #include <util/delay.h>
+
+
+
+#define INF 1e9
+#define GAME_DELAY_VAL 10 // more delay_val , more delay
+#define JUMP_DELAY_VAL 5
+#define JUMP_HEIGHT 5 // more JUMP_LHEIGHT more height
+#define SIAM_HEIGHT 3
 
 
 int pattern[16][16] ;
 
 const int totGreenBar = 180;
 int nowCol;
-int delay;
+int game_delay , jump_delay;
 int currentGreenBar;
 
 void enableRed() {
@@ -45,16 +52,17 @@ void printScreen() {
 				enableGreen();
 				
 			}
-			
 			else if( pattern[r][c] == 2 ){
 				disableGreen();
 				PORTD = (c);
 				enableRed();
 			}
+			else disableGreen();
 			
 		}
 		disableGreen();
 	}
+	//disableGreen();
 }
 
 void printErrorBar() {
@@ -90,12 +98,16 @@ struct greenBars {
 	int len , ro , col;
 } bars[180];
 
+struct Player {	
+	int baseRo , baseCol , jmpLeft , alive;
+} siam;
+
 
 void setGreenBars() {
 		
 	for(int i =0 ; i < totGreenBar ; i++ ) {
 		if(i %3 == 0 ) bars[i].ro = 14;
-		else if(i %3 == 1 ) bars[i].ro = 9;
+		else if(i %3 == 1 ) bars[i].ro = 10;
 		else if(i %3 == 2 ) bars[i].ro = 6;
 		
 		if( i > 0  ) bars[i].col = bars[i-1].col + bars[i-1].len ;
@@ -107,6 +119,13 @@ void setGreenBars() {
 	}
 }
 
+void setSiam() {
+	
+	siam.alive = 1;
+	siam.baseRo = bars[0].ro - 1;
+	siam.baseCol = bars[0].col + 1;
+	siam.jmpLeft = 0;
+}
 
 int insideFrame(int barID) {
 	
@@ -141,9 +160,6 @@ void setOnPatternArray(int barID) {
 	for(int i = printFrm ; i <= printTo ; i++ ) {
 		pattern[barRo][i - nowCol] = 1;
 	}
-	
-	
-	
 }
 
 void UpdateGreenBars() {
@@ -163,6 +179,55 @@ void UpdateGreenBars() {
 }
 
 
+int right_move_condition_found() {
+	
+	if(siam.alive == 0 ) return 0;
+	
+	int ret = 1;
+	for(int i =0 ; i < SIAM_HEIGHT ; i++ ) {
+		if(pattern[siam.baseRo-i][siam.baseCol + 1] == 1 ) ret = 0;
+	}
+	
+	if( ret == 0 )  return 0;
+	
+	return  (bit_is_set(PINB, 0) && game_delay == 1 );
+}
+
+
+void jumpCheckSiam() {
+	
+	
+	if(jump_delay != 1) return;
+	
+	if(siam.jmpLeft > 0) {
+		siam.baseRo--;
+		siam.jmpLeft--;
+		
+		for(int i =0 ; i < SIAM_HEIGHT ; i++ ) {
+			if(pattern[siam.baseRo-i][siam.baseCol] == 1 ) {
+				siam.jmpLeft = 0;
+				siam.baseRo++;
+			}				
+		}		
+	}
+	else {
+		if(pattern[siam.baseRo+1][siam.baseCol] == 0) siam.baseRo++;
+	}	
+	if(siam.baseRo > 15) siam.baseRo = 15; //	DELETE THIS
+	if(siam.baseRo - SIAM_HEIGHT + 1 < 0 ) siam.baseRo = SIAM_HEIGHT -1;
+	
+}
+void UpdateSiam() {
+	
+	if(siam.alive == 0) return;
+	
+	jumpCheckSiam();
+	for(int i =0 ; i < SIAM_HEIGHT; i ++ ) {
+		
+		pattern[siam.baseRo-i][siam.baseCol] = 2;
+	}
+}
+
 void portInit() {
 	
 		
@@ -175,17 +240,27 @@ void portInit() {
 	//PORTC -> last 2 bit controls color C0 -> greeen C1 -> red
 	DDRC = 0xFF;
 	
-	
-	
-	
-	DDRB = 0b11111110;	
+	//PORTB -> b0 = input for left right navigation ; b1 =  input for jump
+	DDRB = 0b11111100;	
 }
 
-int move_condition_found() {
-	delay++;
-	delay %= 5;	
-	return  (bit_is_set(PINB, 0) && delay == 1 );
+
+int jump_condition_found() {
+	
+	if(siam.alive && pattern[siam.baseRo + 1][siam.baseCol] == 1 && bit_is_set(PINB, 1) && jump_delay == 1 ) return 1;
+	if(bit_is_set(PINB, 1) && jump_delay == 1 &&  siam.baseRo == 15) return 1; //DELETE THIS
+	return 0;
 }
+
+void jumpUp() {
+	siam.jmpLeft = JUMP_HEIGHT;
+}
+
+
+void moveRight() { 
+	nowCol++;
+}
+
 
 int main()
 {
@@ -193,19 +268,28 @@ int main()
 	portInit();	
 	
 	setGreenBars();
+	setSiam();
 	
-	
-	
-	
-			
 	while(1) {
+		game_delay++;
+		jump_delay++;
+		game_delay %= GAME_DELAY_VAL;	
+		jump_delay %= JUMP_DELAY_VAL;
 		
 		UpdateGreenBars();
+		UpdateSiam();//ALWAYS UPDATE SIAM AFTER GREEN BARS
+		
 		printScreen();
 		
-		if(move_condition_found()) {
-			nowCol++;
+		if(right_move_condition_found()) {
+			moveRight();
 		}
+		printScreen();
+		if(jump_condition_found()) {
+			jumpUp();
+		}
+		
+		printScreen();
 		
 	}
 
